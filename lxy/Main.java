38096647cbaa68706d160e0f69e5b84286f96c9b
liuxiangyu <1793881416@qq.com>
2022-11-09 22:26:17 +0800

    github提交测试

---
+++ lxy/Main.java
@@ -0,0 +1,14 @@
+package lxy;
+
+/**
+ * @author liuxiangyu
+ * @data 2022 2022/11/9
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println();
+        System.out.println("Hello World!lxy");
+
+    }
+
+}
